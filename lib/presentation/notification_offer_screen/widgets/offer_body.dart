694f04010eea4508b437fff534bad798d1a6c51c
Shehab-adel <shoopa5050@gmail.com>
2024-02-01 19:27:05 +0200

    Modified offer_screen Ui

---
+++ lib/presentation/notification_offer_screen/widgets/offer_body.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:rebuy/core/app_export.dart';
+
+class OfferBody extends StatelessWidget {
+  const OfferBody(
+      {Key? key,
+      required this.offerTitle,
+      required this.offerDescription,
+      required this.offerTime})
+      : super(key: key);
+  final String offerTitle;
+  final String offerDescription;
+  final String offerTime;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.all(16.h),
+        decoration: AppDecoration.fillOnPrimaryContainer,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomImageView(
+                  imagePath: AppImageConstants.imgOffer,
+                  height: 24.adaptSize,
+                  width: 24.adaptSize,
+                  margin: EdgeInsets.only(bottom: 94.v)),
+              Expanded(
+                  child: Padding(
+                      padding: EdgeInsets.only(left: 12.h),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(offerTitle,
+                                style: theme.textTheme.titleSmall!.copyWith(
+                                    color: theme.colorScheme.onPrimary
+                                        .withOpacity(1))),
+                            SizedBox(height: 10.v),
+                            SizedBox(
+                                width: 305.h,
+                                child: Text(offerDescription,
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: theme.textTheme.bodySmall!.copyWith(
+                                        color: appTheme.blueGray300,
+                                        height: 1.80))),
+                            SizedBox(height: 9.v),
+                            Text(offerTime,
+                                style: CustomTextStyles.bodySmallOnPrimary10
+                                    .copyWith(
+                                        color: theme.colorScheme.onPrimary
+                                            .withOpacity(1)))
+                          ])))
+            ]));
+  }
+}
