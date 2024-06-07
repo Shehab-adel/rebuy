af0605cdc1cd805d77165968d84bd69d45e4e39b
Shehab-adel <shoopa5050@gmail.com>
2024-06-07 14:21:32 +0300

    Refactor ChooseCreditOrDebitCardScreen widget
    Simplified the ChooseCreditOrDebitCardScreen widget by removing unnecessary methods and using a ListView.separated instead of a CarouselSlider. Also, replaced onTapArrowLeft and onTapPayButton methods with Navigator.pop and Navigator

--- lib/presentation/choose_credit_or_debit_card_screen/choose_credit_or_debit_card_screen.dart
+++ lib/presentation/choose_credit_or_debit_card_screen/choose_credit_or_debit_card_screen.dart
@@ -10,97 +10,43 @@ import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../choose_credit_or_debit_card_screen/widgets/creditcard_item_widget.dart';
 
-// ignore_for_file: must_be_immutable
 class ChooseCreditOrDebitCardScreen extends StatelessWidget {
-  ChooseCreditOrDebitCardScreen({Key? key}) : super(key: key);
-
-  int sliderIndex = 1;
+  const ChooseCreditOrDebitCardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
-    return SafeArea(
-        child: Scaffold(
-            appBar: _buildAppBar(context),
-            body: Container(
-                width: double.maxFinite,
-                padding: EdgeInsets.symmetric(horizontal: 16.h, vertical: 27.v),
-                child: Column(children: [
-                  _buildCreditCard(context),
-                  SizedBox(height: 16.v),
-                  SizedBox(
-                      height: 8.v,
-                      child: AnimatedSmoothIndicator(
-                          activeIndex: sliderIndex,
-                          count: 1,
-                          axisDirection: Axis.horizontal,
-                          effect: ScrollingDotsEffect(
-                              spacing: 8,
-                              activeDotColor:
-                                  theme.colorScheme.primary.withOpacity(1),
-                              dotColor: appTheme.blue50,
-                              dotHeight: 8.v,
-                              dotWidth: 8.h))),
-                  SizedBox(height: 5.v)
-                ])),
-            bottomNavigationBar: _buildPayButton(context)));
-  }
-
-  /// Section Widget
-  PreferredSizeWidget _buildAppBar(BuildContext context) {
-    return CustomAppBar(
-        leadingWidth: 40.h,
-        leading: AppbarLeadingImage(
-            imagePath: AppImageConstants.imgArrowLeftBlueGray300,
-            margin: EdgeInsets.only(left: 16.h, top: 15.v, bottom: 16.v),
-            onTap: () {
-              onTapArrowLeft(context);
-            }),
-        title: AppbarSubtitle(
-            text: "Choose Card", margin: EdgeInsets.only(left: 12.h)),
-        actions: [
-          AppbarTrailingImage(
-              imagePath: AppImageConstants.imgPlusIcon,
-              margin: EdgeInsets.fromLTRB(16.h, 15.v, 16.h, 16.v))
-        ]);
-  }
-
-  /// Section Widget
-  Widget _buildCreditCard(BuildContext context) {
-    return CarouselSlider.builder(
-        options: CarouselOptions(
-            height: 190.v,
-            initialPage: 0,
-            autoPlay: true,
-            viewportFraction: 1.0,
-            enableInfiniteScroll: false,
-            scrollDirection: Axis.horizontal,
-            onPageChanged: (index, reason) {
-              sliderIndex = index;
-            }),
-        itemCount: 1,
-        itemBuilder: (context, index, realIndex) {
-          return CreditcardItemWidget();
-        });
-  }
-
-  /// Section Widget
-  Widget _buildPayButton(BuildContext context) {
-    return CustomElevatedButton(
-        text: "Pay 766.86",
-        margin: EdgeInsets.only(left: 16.h, right: 16.h, bottom: 50.v),
-        onPressed: () {
-          onTapPayButton(context);
-        });
-  }
-
-  /// Navigates back to the previous screen.
-  onTapArrowLeft(BuildContext context) {
-    Navigator.pop(context);
-  }
-
-  /// Navigates to the successScreen when the action is triggered.
-  onTapPayButton(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.successScreen);
+    return Scaffold(
+        appBar: CustomAppBar(
+            leadingWidth: 40.h,
+            leading: AppbarLeadingImage(
+                imagePath: AppImageConstants.imgArrowLeftBlueGray300,
+                margin: EdgeInsets.only(left: 16.h, top: 15.v, bottom: 16.v),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            title: AppbarSubtitle(
+                text: "Choose Card", margin: EdgeInsets.only(left: 12.h)),
+            actions: [
+              AppbarTrailingImage(
+                  imagePath: AppImageConstants.imgPlusIcon,
+                  margin: EdgeInsets.fromLTRB(16.h, 15.v, 16.h, 16.v))
+            ]),
+        body: Container(
+            width: double.maxFinite,
+            padding: EdgeInsets.symmetric(horizontal: 16.h, vertical: 27.v),
+            child: ListView.separated(
+                itemCount: 3,
+                separatorBuilder: (context, index) {
+                  return SizedBox(height: 20.v);
+                },
+                itemBuilder: (context, index) {
+                  return CreditcardItemWidget();
+                })),
+        bottomNavigationBar: CustomElevatedButton(
+            text: "Pay 766.86",
+            margin: EdgeInsets.only(left: 16.h, right: 16.h, bottom: 50.v),
+            onPressed: () {
+              Navigator.pushNamed(context, AppRoutes.successScreen);
+            }));
   }
 }
